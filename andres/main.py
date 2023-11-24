01eea2c1d957a2dc8c2d20c3e7574667424d0a09
Lenin Acosta <rkb.lenin@gmail.com>
2023-11-24 00:46:30 -0500

    Update main.py

--- andres/main.py
+++ andres/main.py
@@ -3,20 +3,42 @@ import time
 
 def main(page: flet.Page):
     page.title= "Light spectre controled with python"
+    file = flet.Text()
+    def filepicked(e:flet.FilePickerResultEvent):
+        if e.files:
+            r = list(map(lambda f: f.name, e.files))
+            file.value = r[0]
+            file.update()
+    file_picker_dialog = flet.FilePicker(on_result=filepicked)
+    page.overlay.append(file_picker_dialog)
     col = flet.Column(
         controls=[
             flet.Row(
+                spacing= 5,
                 controls=[
-                    flet.Text(value="Doing andres homework"),
-                    flet.Slider(
-                        
+                    flet.Column(
+                        spacing= 5,
+                        controls=[
+                            file,
+                            flet.OutlinedButton(
+                                icon= flet.icons.FILE_UPLOAD,
+                                text= "Upload image",
+                                on_click= lambda _: file_picker_dialog.pick_files()
+                            ),
+                        ]
                     ),
+                    flet.Column(
+                        controls=[
+                            flet.Text(value="Hayase <3")
+                        ]
+                    )
                 ],
             ),
             flet.Column(
                 controls=[
+                    file,
                     flet.Image(
-                        src=f"/images/arabian_cat.png",
+                        src=f"/images/AOT.png",
                         width=100,
                         height=100,
                         fit=flet.ImageFit.CONTAIN,
